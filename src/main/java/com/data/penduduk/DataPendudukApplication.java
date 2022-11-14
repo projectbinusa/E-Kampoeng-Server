713652615acc81e01d5484827e23f1521465bef2
syarifahaha <laylarobiatussyarifah.tkj.2021@binusasmg.sch.id>
2022-11-14 11:06:24 +0700

    start project

---
+++ src/main/java/com/data/penduduk/DataPendudukApplication.java
@@ -0,0 +1,13 @@
+package com.data.penduduk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataPendudukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataPendudukApplication.class, args);
+	}
+
+}
