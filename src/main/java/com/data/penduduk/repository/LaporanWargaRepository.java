ab833b3413f277360b7192b1e89d5f620f1cc37b
syarifahaha <laylarobiatussyarifah.tkj.2021@binusasmg.sch.id>
2022-11-17 11:58:53 +0700

    update laporan_warga by Layla

---
+++ src/main/java/com/data/penduduk/repository/LaporanWargaRepository.java
@@ -0,0 +1,7 @@
+package com.data.penduduk.repository;
+
+import com.data.penduduk.model.LaporanWarga;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LaporanWargaRepository extends JpaRepository<LaporanWarga, Long> {
+}
---
+++ src/main/java/com/data/penduduk/service/LaporanWargaService.java
@@ -0,0 +1,41 @@
+package com.data.penduduk.service;
+
+import com.data.penduduk.model.LaporanWarga;
+import com.data.penduduk.repository.LaporanWargaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.Date;
+import java.util.List;
+
+@Service
+public class LaporanWargaService {
+
+    @Autowired
+    LaporanWargaRepository laporanWargaRepository;
+
+    public List<LaporanWarga> getAllLaporan() {
+        return laporanWargaRepository.findAll();
+    }
+
+    public LaporanWarga getLaporanById(Long id) {
+        return laporanWargaRepository.findById(id).orElse(null);
+    }
+
+    public LaporanWarga createLaporan(LaporanWarga laporanWarga) {
+        return laporanWargaRepository.save(laporanWarga);
+    }
+
+    public LaporanWarga editLaporan(Long id, String nama_pelapor, String judul_laporan, String laporan, Date tgl_laporan) {
+        LaporanWarga laporanWarga = laporanWargaRepository.findById(id).orElse(null);
+        laporanWarga.setNama_pelapor(nama_pelapor);
+        laporanWarga.setJudul_laporan(judul_laporan);
+        laporanWarga.setLaporan(laporan);
+        laporanWarga.setTgl_laporan(tgl_laporan);
+        return laporanWargaRepository.save(laporanWarga);
+    }
+
+    public void deleteLaporan(Long id) {
+        laporanWargaRepository.deleteById(id);
+    }
+}
